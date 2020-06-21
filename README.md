fe908797162adf3faca7359c6a0d7d0f676ebbf5
Mario Bianucci <bianucci1@gmail.com>
2020-06-20 23:34:54 -0700

    Expand to PPDG and add a check to confirm the correct button is being clicked

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 Extension for Firefox and Chromium based browsers to skip the stupid envelope animation prior to being able to view a gift card from MyGiftCardsPlus.
 
+Also works for automatically clicking the "Skip" button on Paypal Digital Gift gift cards.
+
 You may still have to solve a captcha, but at least you don't have to watch the same animation over and over!
--- skip.js
+++ skip.js
@@ -1,1 +1,18 @@
-document.querySelector('.btn_base').click();
+function skip(button, inner_text) {
+    let buttons = document.querySelectorAll(button);
+
+    // Confirm we aren't about to click on something we don't intend to.
+    for(let i = 0;i < buttons.length; ++i) {
+        if(buttons[i].innerText === inner_text)
+            buttons[i].click();
+    }
+}
+
+switch (window.location.hostname) {
+    case 'delivery.cardago.com':
+        skip('.btn_base', 'READY TO OPEN YOUR GIFT CARD?');
+        break;
+    case 'www.paypal.com':
+        skip('[data-pa-click]', 'Skip');
+        break;
+}
