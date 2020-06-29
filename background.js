b2db8ffbe9e2465fae5890460185ad25a3907671
Mario Bianucci <bianucci1@gmail.com>
2020-06-28 22:39:00 -0700

    Add FF support, update popup html, update version number

--- background.js
+++ background.js
@@ -1,15 +1,9 @@
-// Listens for the URLs that the GC are available on and enables the extension button
-// when the active tab is on either site.
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {urlContains: 'www.paypal.com/gifts/claim/'},
-      }), new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {urlContains: 'www.vcdelivery.com/certificate/'},
-      })
-      ],
-          actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-  });
+// Listens for the URLs that the GC are available on and enables the extension
+// button when the active tab is on either site.
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  if (changeInfo.status === 'complete' &&
+     (tab.url.includes('www.paypal.com/gifts/claim/') ||
+      tab.url.includes('www.vcdelivery.com/certificate/'))) {
+    chrome.pageAction.show(tabId);
+  }
 });
